c8307bcbf378f6246d7a16898eaaee31db631505
Jay Patel <jy439129@dal.ca>
2021-11-24 15:57:15 -0400

    Created project structure

---
+++ src/main/java/com/centdb/CentDbApplication.java
@@ -0,0 +1,13 @@
+package com.centdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentDbApplication.class, args);
+	}
+
+}
